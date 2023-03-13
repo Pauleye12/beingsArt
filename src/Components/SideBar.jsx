2d151784e2bc71f900cb8b82df26cd79ac6c714d
pauleye <orepaul12@gmail.com>
2023-03-13 22:11:56 +0100

    side bar

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -29,8 +29,28 @@ function SideBar() {
     },
   };
 
+  const rocketVariant = {
+    inView: {
+      y: [0, -10, 5, -10, 5, -100],
+      opacity: [1, 1, 1, 1, 1, 0],
+      transition: {
+        duration: 2,
+        repeat: Infinity,
+        ease: "easeInOut",
+        delay: 0.3,
+        repeatDelay: 2,
+      },
+    },
+  };
+
   return (
-    <motion.div variants={SideBarVariant} initial='hidden' animate='visible' exit='exit' className="bg-[#3d3c70] fixed top-0 mt-[74px] right-0 left-0 w-screen flex justify-center items-center z-[-100] pt-[10px] pb-[25px]">
+    <motion.div
+      variants={SideBarVariant}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      className="bg-[#3d3c70] fixed top-0 mt-[74px] right-0 left-0 w-screen flex justify-center items-center z-[-100] pt-[10px] pb-[25px]"
+    >
       <div>
         <ul className="nav_links font-bold flex justify-center items-center gap-[25px] flex-col">
           <li>
@@ -42,8 +62,24 @@ function SideBar() {
           <li>
             <a href="">Vision</a>
           </li>
-          <li className="academy_link font-bold px-[17px] py-[10px] rounded-3xl border-[3px] border-[#f7c5ff]">
-            <a href="">Academy</a>
+          <li className="academy_link font-bold px-[17px] py-[8px]  flex justify-center items-center rounded-3xl border-[3px] border-[#f7c5ff]">
+            <a
+              className="  "
+              href="http://"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="flex justify-center items-center">
+                <p>Academy</p>
+                <motion.div
+                  variants={rocketVariant}
+                  whileInView="inView"
+                  className="academy_img "
+                >
+                  <img src="./Beings Web Assets/rocket.svg" alt="" />
+                </motion.div>
+              </div>
+            </a>
           </li>
         </ul>
       </div>
